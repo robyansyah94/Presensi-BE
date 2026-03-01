bc105decd9cebc20f577862e10f83243e6dad217
robyansyah94 <obys094@gmail.com>
2026-03-01 20:27:13 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,22 @@
 use App\Http\Controllers\Api\PresensiController;
 use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::middleware('auth:sanctum')->post('/presensi/scan', [PresensiController::class, 'scan']);
 
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
+
+    $user = $request->user()->load('karyawan.jabatan');
+
+    return response()->json([
+        'id' => $user->id,
+        'name' => $user->name,
+        'email' => $user->email,
+        'nip' => optional($user->karyawan)->nip,
+        'foto' => optional($user->karyawan)->foto,
+        'jabatan' => optional(optional($user->karyawan)->jabatan)->nama_jabatan,
+    ]);
+});
